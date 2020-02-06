1c1dfee1118818e51ad7f203f83df6488e66d4d4
nir-welldone <32127792+nir-welldone@users.noreply.github.com>
2020-02-06 17:58:08 +0200

    init templatesDirectory

--- cli/commands/init.js
+++ cli/commands/init.js
@@ -3,22 +3,23 @@ import { sync } from 'cross-spawn';
 import * as logger from '../utils/logger';
 import { hasYarn, retrievePackageJson, writePackageJson } from '../utils/packageManager';
 
-export default async (options = {}) => {
+export default async ({ useNpm, templatesDirectory }) => {
   logger.title('\n Adding Muil to project... \n');
 
   logger.info('Installing package...');
-  const useYarn = Boolean(options.useNpm !== true) && hasYarn();
+  const useYarn = Boolean(useNpm !== true) && hasYarn();
   const installAsDevDependencies = true;
   sync(useYarn ? 'yarn' : 'npm', [useYarn ? 'add' : 'install', 'muil-2', installAsDevDependencies ? '-D' : '']);
 
   logger.info('Adding scripts...\n');
+  const templatesDirectoryArg = templatesDirectory ? ` -d ${templatesDirectory}` : '';
   const packageJson = await retrievePackageJson();
   packageJson.dependencies = packageJson.dependencies || {};
   packageJson.devDependencies = packageJson.devDependencies || {};
   packageJson.scripts = packageJson.scripts || {};
-  packageJson.scripts.muil = 'muil editor';
+  packageJson.scripts.muil = `muil editor${templatesDirectoryArg}`;
   packageJson.scripts['muil-login'] = 'muil login';
-  packageJson.scripts['muil-publish'] = 'muil publish';
+  packageJson.scripts['muil-publish'] = `muil publish${templatesDirectoryArg}`;
   writePackageJson(packageJson);
 
   logger.success('Muil installed successfully 💪');
--- cli/generate.js
+++ cli/generate.js
@@ -9,6 +9,7 @@ program
   .command('init')
   .description('Initialize Muil into project.')
   .option('-n --use-npm', 'Use npm to install deps')
+  .option('-d --templatesDirectory <templatesDirectory>', "Templates root directory, default: 'templates'")
   .action(options => initiate(options));
 
 program
