e6e39ee28c5d08d5ac94a1bc9269d735a8712380
Shahaf Shaked <shahafs@welldone-software.com>
2020-02-20 14:17:59 +0200

    remove templates

--- templates/Template5.template.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './App.css';
-
-const Template5 = () => <div className="app">Template useCSS</div>;
-
-Template5.displayName = 'Template use CSS';
-
-export default Template5;
