6b8e3c73c00c04a36f7f13389cd4f0e014dd2f90
nir905 <nira@welldone-software.com>
2020-02-23 18:18:19 +0200

    select first template

--- packages/editor-manager/src/hooks/useTemplates.js
+++ packages/editor-manager/src/hooks/useTemplates.js
@@ -1,15 +1,22 @@
 import { useState, useCallback, useEffect } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
 const useTemplates = () => {
   const [defaultTemplates, setDefaultTemplates] = useState({});
+  const history = useHistory();
+  const location = useLocation();
 
   const handleReceiveMessage = useCallback(
     ({ data }) => {
       if (data?.templates && !Object.keys(defaultTemplates).length) {
         setDefaultTemplates(data.templates);
+
+        if (location.pathname === '/') {
+          history.push(Object.values(data.templates)?.[0]?.id || '/');
+        }
       }
     },
-    [defaultTemplates],
+    [defaultTemplates, history, location],
   );
 
   useEffect(() => {
