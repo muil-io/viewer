bd3847458339c83b622b8c38e95c649d6a4ace65
Shahaf Shaked <shahafs@welldone-software.com>
2020-02-26 16:53:02 +0200

    cli

--- packages/editor/scripts/generate.js
+++ packages/editor/scripts/generate.js
@@ -2,21 +2,13 @@ import program from 'commander';
 import editor from './editor';
 
 program
-  .command('editor')
-  .description('Starting Muil editor')
+  .name('muil')
   .option('-p --port <port>', 'Editor port, default: 8000', 8000)
   .option(
     '-d --templatesDirectory <templatesDirectory>',
     "Templates root directory, default: './templates'",
     './templates',
   )
-  .action(options => editor(options));
-
-program
-  .name('muil')
-  .usage('<command> [options]')
   .parse(process.argv);
 
-if (program.rawArgs.length < 3) {
-  program.help();
-}
+editor(program);
