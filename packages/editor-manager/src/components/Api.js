b02f5642a905781f2e2a31e449db46f849912f35
nir905 <nira@welldone-software.com>
2020-02-26 17:23:13 +0200

    Update Api.js

--- packages/editor-manager/src/components/Api.js
+++ packages/editor-manager/src/components/Api.js
@@ -6,43 +6,64 @@ import 'ace-builds/src-noconflict/theme-textmate';
 import CopyIcon from '../assets/copy.svg';
 
 const Wrapper = styled.div`
-  height: calc(100% - 120px);
+  padding: 20px;
+  background: #fff;
+  margin-top: -10px;
+  height: calc(100% - 100px);
+
   .ace-tm {
-    background-color: transparent;
+    background: #f8f8f8;
+    border: 1px solid #cccccc;
+    border-radius: 5px;
+    transition: border-color 200ms;
   }
+`;
+
+const Title = styled.div`
+  color: #adadad;
+  margin-bottom: 8px;
+  font-size: 14px;
+`;
+
+const InputRow = styled.div`
+  position: relative;
+`;
+
+const Method = styled.div`
+  color: green;
+  font-weight: bold;
+  position: absolute;
+  left: 6px;
+  top: 11px;
+  font-size: 11px;
+`;
+
+const Input = styled.input.attrs(() => ({ readOnly: true }))`
+  border: 1px solid #cccccc;
+  color: #424242;
+  padding: 10px 39px;
+  border-radius: 5px;
+  width: 100%;
+  background: #f8f8f8;
+  margin-bottom: 15px;
+  outline: none;
+  transition: border-color 200ms;
 `;
 
 const CopyButton = styled(CopyIcon)`
   position: absolute;
-  right: 20px;
-  top: 26px;
+  right: 8px;
+  top: 7px;
   cursor: pointer;
-  display: none;
-`;
 
-const Head = styled.div`
-  position: relative;
-  padding: 29px 20px;
-  margin: -10px 0 10px 0;
-  background: #fff;
-  box-shadow: 0 0 8px 1px rgba(0, 0, 0, 0.2);
-
-  > div {
-    &:first-child {
-      color: green;
-      font-weight: bold;
-      margin-bottom: 8px;
-    }
-
-    &:last-child {
-      color: #4688ff;
-      word-break: break-word;
-    }
+  > path {
+    transition: 200ms;
+    fill: #cccccc;
   }
 
   &:hover {
-    ${CopyButton} {
-      display: block;
+    > path {
+      fill: #6735e0;
     }
   }
 `;
@@ -52,11 +73,15 @@ const Api = ({ dynamicProps, id }) => {
 
   return (
     <Wrapper>
-      <Head>
-        <div>POST</div>
+      <Title>Request Name</Title>
+
+      <InputRow>
+        <Method>POST</Method>
+        <Input value={url} />
         <CopyButton onClick={() => navigator.clipboard.writeText(url)} />
-        <div>{url}</div>
-      </Head>
+      </InputRow>
+
+      <Title>Request Body</Title>
 
       <AceEditor
         mode="json"
