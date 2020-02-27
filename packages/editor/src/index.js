d0ae9d58a2a8fae2fee3beeef93f111d71933123
Shahaf Shaked <shahafs@welldone-software.com>
2020-02-27 18:28:20 +0200

    Update index.js

--- packages/editor/src/index.js
+++ packages/editor/src/index.js
@@ -6,9 +6,12 @@ import { getTemplateFromUrl, getTemplatesForParent } from './utils/templates';
 
 const templates = getTemplates();
 // eslint-disable-next-line no-restricted-globals
-parent.postMessage({
-  templates: getTemplatesForParent(templates),
-});
+parent.postMessage(
+  {
+    templates: getTemplatesForParent(templates),
+  },
+  '*',
+);
 
 const App = () => {
   const { Template, dynamicProps } = useMemo(
