93217ca4df7542d7897868d9b31868e6c92d7987
nir905 <nira@welldone-software.com>
2020-03-01 15:49:12 +0200

    install babel-loader when using cra

--- packages/cli/src/utils/missingDependencies.js
+++ packages/cli/src/utils/missingDependencies.js
@@ -1,6 +1,17 @@
 const MUST_INCLUDE_DEPENDENCIES = ['react', 'react-dom'];
 
-const missingDependencies = packageJson =>
-  MUST_INCLUDE_DEPENDENCIES.filter(dependency => !packageJson.dependencies || !packageJson.dependencies[dependency]);
+const missingDependencies = packageJson => {
+  const missing = MUST_INCLUDE_DEPENDENCIES.filter(
+    dependency => !packageJson.dependencies || !packageJson.dependencies[dependency],
+  );
+
+  const isReactScriptsInstalled = packageJson.dependencies && packageJson.dependencies['react-scripts'];
+
+  if (isReactScriptsInstalled) {
+    return [...missing, 'babel-loader'];
+  }
+
+  return missing;
+};
 
 export default missingDependencies;
