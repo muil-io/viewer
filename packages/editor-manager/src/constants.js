e03997deae5f3e707d66bde8e066ea9ee93537c2
nir905 <nira@welldone-software.com>
2020-03-09 12:07:49 +0200

    remove big screen option

--- packages/editor-manager/src/constants.js
+++ packages/editor-manager/src/constants.js
@@ -1,4 +1,3 @@
-import FullIcon from './assets/sizes/full.svg';
 import MobileIcon from './assets/sizes/mobile.svg';
 import MonitorIcon from './assets/sizes/monitor.svg';
 import TabletIcon from './assets/sizes/tablet.svg';
@@ -6,8 +5,7 @@ import TabletIcon from './assets/sizes/tablet.svg';
 export const SCREEN_SIZES = {
   small: { size: '360px', icon: MobileIcon },
   medium: { size: '640px', icon: TabletIcon },
-  big: { size: '870px', icon: MonitorIcon },
-  full: { size: 'calc(100% - 160px)', icon: FullIcon },
+  full: { size: 'calc(100% - 160px)', icon: MonitorIcon },
 };
 
 export const HEADER_HEIGHT = 80;
