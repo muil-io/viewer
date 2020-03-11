e6feb6d58a56dcc29a66e613a21d6c0f3e77ebeb
nir905 <nira@welldone-software.com>
2020-03-11 09:08:53 +0200

    ignore templates with errors

--- packages/editor/src/utils/getTemplates.js
+++ packages/editor/src/utils/getTemplates.js
@@ -4,18 +4,22 @@ export default () => {
     const templateFiles = templateContext.keys();
 
     return templateFiles.reduce((templates, templateFileName) => {
-      const templateName = templateFileName.split('/');
-      const templateId = templateName[templateName.length - 1].replace(/.template.js/g, '').replace(/\.\//g, '');
-      const Template = templateContext(templateFileName).default;
-      return {
-        ...templates,
-        [templateId]: {
-          id: templateId,
-          name: Template.displayName || templateFileName,
-          Template,
-          dynamicProps: Template.dynamicProps,
-        },
-      };
+      try {
+        const templateName = templateFileName.split('/');
+        const templateId = templateName[templateName.length - 1].replace(/.template.js/g, '').replace(/\.\//g, '');
+        const Template = templateContext(templateFileName).default;
+        return {
+          ...templates,
+          [templateId]: {
+            id: templateId,
+            name: Template.displayName || templateFileName,
+            Template,
+            dynamicProps: Template.dynamicProps,
+          },
+        };
+      } catch (err) {
+        return templates;
+      }
     }, {});
   } catch {
     return {};
