d5db4eb3efe6f4f081978648f526676c1e4e54e3
nir905 <nira@welldone-software.com>
2020-03-15 16:24:09 +0200

    Create index.js

---
+++ packages/charts/src/index.js
@@ -0,0 +1,3 @@
+export { default as BarChart } from './BarChart';
+export { default as CompressionChart } from './CompressionChart';
+export { default as HorizontalBarChart } from './HorizontalBarChart';
