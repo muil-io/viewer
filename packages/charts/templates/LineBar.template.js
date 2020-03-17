dfab9c7a780661da7ef741e3242d4ca246f43e81
nir905 <nira@welldone-software.com>
2020-03-17 09:23:27 +0200

    add LineBar

---
+++ packages/charts/templates/LineBar.template.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import BaseLineBar from '../src/LineBar';
+import Layout from './Layout';
+
+const Text = styled.div`
+  font-family: Arial;
+`;
+
+const StyledLineBar = styled(BaseLineBar)``;
+
+const LineBar = ({ percents }) => (
+  <Layout>
+    <Text>Line Bar</Text>
+
+    <StyledLineBar percents={percents} />
+  </Layout>
+);
+
+LineBar.displayName = 'Line Bar';
+
+LineBar.dynamicProps = {
+  percents: [
+    { value: 10, color: '#17a2b8' },
+    { value: 20, color: '#d5d5d4' },
+    { value: 70, color: '#90ED7D' },
+  ],
+};
+
+export default LineBar;
