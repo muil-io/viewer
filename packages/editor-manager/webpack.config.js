e37ac48da707b8cb67cb1eacb237ff698b4e7b70
Shahaf Shaked <shahafs@welldone-software.com>
2020-04-18 17:23:28 +0300

    Editor - support static props in class

--- packages/editor-manager/webpack.config.js
+++ packages/editor-manager/webpack.config.js
@@ -29,6 +29,7 @@ const common = () => ({
             presets: ['@babel/preset-env', '@babel/preset-react'],
             plugins: [
               ['react-css-modules', { generateScopedName: '[local]___[hash:base64:5]' }],
+              '@babel/plugin-proposal-class-properties',
               '@babel/plugin-proposal-nullish-coalescing-operator',
               '@babel/plugin-proposal-optional-chaining',
             ],
--- packages/editor/webpack.config.js
+++ packages/editor/webpack.config.js
@@ -34,7 +34,12 @@ module.exports = ({ templatesDirectory, babelrc }) => ({
           loader: 'babel-loader',
           options: babelrc || {
             presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: [['react-css-modules', { generateScopedName: '[local]___[hash:base64:5]' }]],
+            plugins: [
+              ['react-css-modules', { generateScopedName: '[local]___[hash:base64:5]' }],
+              '@babel/plugin-proposal-class-properties',
+              '@babel/plugin-proposal-nullish-coalescing-operator',
+              '@babel/plugin-proposal-optional-chaining',
+            ],
           },
         },
       },
