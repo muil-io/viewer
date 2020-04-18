8e27a424f3bfb6361dea0066d53f17f913eb7eca
Shahaf Shaked <shahafs@welldone-software.com>
2020-04-18 17:13:43 +0300

    fix logger to work when stdout is not tty

--- packages/cli/src/utils/logger.js
+++ packages/cli/src/utils/logger.js
@@ -1,12 +1,16 @@
 import chalk from 'chalk';
+import readline from 'readline';
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
 export const title = message => console.log(`${chalk.inverse(message)}`);
 
 export const info = message => process.stdout.write(`${chalk.cyan(' •')} ${message}`);
 
 export const infoSuccess = () => {
-  process.stdout.cursorTo(0);
+  readline.cursorTo(process.stdout, 0);
   process.stdout.write(`${chalk.green(' ✔')}\n`);
+  rl.close();
 };
 
 export const success = message => console.log(`${chalk.green(' ✔')} ${message || ''}`);
