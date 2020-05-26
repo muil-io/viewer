8c8dc476416896d9ee0e3dd21bf0c717be84c77f
Shahaf Shaked <shahaf@live.com>
2020-05-26 17:52:08 +0300

    remove charts

--- packages/charts/src/index.js
+++
@@ -1,4 +0,0 @@
-export { default as BarChart } from './BarChart';
-export { default as Calendar } from './Calendar';
-export { default as HorizontalBarChart } from './HorizontalBarChart';
-export { default as LineBar } from './LineBar';
