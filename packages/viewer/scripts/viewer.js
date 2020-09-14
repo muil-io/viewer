3fa8a792a9889ec3308cf154e0cb561e6e703864
nir905 <nir905@gmail.com>
2020-09-14 12:28:46 +0300

    viewerMiddleware

--- packages/viewer/scripts/viewer.js
+++ packages/viewer/scripts/viewer.js
@@ -1,34 +1,19 @@
-import path from 'path';
-import { existsSync, readFileSync } from 'fs';
 import chalk from 'chalk';
 import express from 'express';
 import open from 'open';
-import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
-import webpack from 'webpack';
-import previewConfig from '../webpack.config';
+import viewerMiddleware from './viewerMiddleware';
 
-const rootDir = process.env.INIT_CWD || __dirname;
-const distDirectory = path.resolve(__dirname, '../lib');
-const configPath = path.resolve(rootDir, '.muil/config.js');
-const babelrcPath = path.resolve(rootDir, '.muil/.babelrc');
 const app = express();
 
 export default async ({ port, templatesDirectory }) => {
   console.log(`${chalk.inverse('\n Starting Muil viewer... \n')}`);
 
-  // eslint-disable-next-line
-  const config = existsSync(configPath) ? require(configPath) : { webpack: (config) => config };
-  const babelrc = existsSync(babelrcPath) ? JSON.parse(readFileSync(babelrcPath, 'utf-8')) : null;
-
-  const defaultCompiler = previewConfig({ templatesDirectory: path.resolve(rootDir, templatesDirectory), babelrc });
-  const finalCompiler = config.webpack(defaultCompiler);
-  const compiler = webpack(finalCompiler);
-  const middleware = new webpackDevMiddleware(compiler, { publicPath: '/', logLevel: 'error' });
+  const { middleware, compiler, viewerDirectory } = viewerMiddleware({ templatesDirectory });
 
   app.use(middleware);
   app.use(webpackHotMiddleware(compiler));
-  app.use(express.static(distDirectory));
+  app.use(express.static(viewerDirectory));
 
   middleware.waitUntilValid(() => {
     app.listen(port, () => console.log(`✨ Muil templates viewer is running at http://localhost:${port}/`));
---
+++ packages/viewer/scripts/viewerMiddleware.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const { existsSync, readFileSync } = require('fs');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpack = require('webpack');
+const previewConfig = require('../webpack.config');
+
+const rootDir = process.env.INIT_CWD || __dirname;
+const distDirectory = path.resolve(__dirname, '../lib');
+const configPath = path.resolve(rootDir, '.muil/config.js');
+const babelrcPath = path.resolve(rootDir, '.muil/.babelrc');
+
+const viewerMiddleware = ({ templatesDirectory }) => {
+  // eslint-disable-next-line
+  const config = existsSync(configPath) ? require(configPath) : { webpack: (config) => config };
+  const babelrc = existsSync(babelrcPath) ? JSON.parse(readFileSync(babelrcPath, 'utf-8')) : null;
+
+  const defaultCompiler = previewConfig({ templatesDirectory: path.resolve(rootDir, templatesDirectory), babelrc });
+  const finalCompiler = config.webpack(defaultCompiler);
+  const compiler = webpack(finalCompiler);
+  const middleware = new webpackDevMiddleware(compiler, { publicPath: '/', logLevel: 'error' });
+
+  return { compiler, middleware, viewerDirectory: distDirectory };
+};
+
+module.exports = viewerMiddleware;
