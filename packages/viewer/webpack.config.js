f4030e06bc6a744d8644efdb226479201e8dc69a
nir905 <nir905@gmail.com>
2020-12-01 09:13:55 +0200

    viewer

--- packages/viewer/webpack.config.js
+++ packages/viewer/webpack.config.js
@@ -11,77 +11,80 @@ const paths = {
   dist: path.resolve(__dirname, 'lib'),
 };
 
-const includedPaths = (templatesDirectory) => [
-  path.resolve(__dirname, 'src'),
-  path.resolve(process.env.INIT_CWD || __dirname, templatesDirectory),
-  path.resolve(process.env.INIT_CWD || __dirname, 'src'),
-];
+module.exports = ({ templatesDirectory, babelrc }) => {
+  const babelLoader = {
+    loader: 'babel-loader',
+    options: babelrc || {
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+      plugins: [
+        ['react-css-modules', { generateScopedName: '[local]___[hash:base64:5]' }],
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-proposal-nullish-coalescing-operator',
+        '@babel/plugin-proposal-optional-chaining',
+      ],
+    },
+  };
 
-module.exports = ({ templatesDirectory, babelrc }) => ({
-  entry: [paths.src, 'webpack-hot-middleware/client'],
-  mode: 'development',
-  devtool: 'cheap-module-source-map',
-  output: {
-    filename: 'iframe.js',
-    path: paths.dist,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: includedPaths(templatesDirectory),
-        use: {
-          loader: 'babel-loader',
-          options: babelrc || {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: [
-              ['react-css-modules', { generateScopedName: '[local]___[hash:base64:5]' }],
-              '@babel/plugin-proposal-class-properties',
-              '@babel/plugin-proposal-nullish-coalescing-operator',
-              '@babel/plugin-proposal-optional-chaining',
-            ],
-          },
+  return {
+    entry: [paths.src, 'webpack-hot-middleware/client'],
+    mode: 'development',
+    devtool: 'cheap-module-source-map',
+    output: {
+      filename: 'iframe.js',
+      path: paths.dist,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: babelLoader,
         },
-      },
-      {
-        test: /\.module\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-              modules: {
-                localIdentName: '[local]___[hash:base64:5]',
+        {
+          test: /\.js$/,
+          include: path.resolve(__dirname, 'src'),
+          use: babelLoader,
+        },
+        {
+          test: /\.module\.css$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                modules: {
+                  localIdentName: '[local]___[hash:base64:5]',
+                },
               },
             },
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        exclude: /\.module\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(eot|otf|woff|woff2|ttf)?$/,
-        use: ['file-loader'],
-      },
-      {
-        test: /\.(bmp|gif|jpe?g|png)?$/,
-        use: ['file-loader'],
-      },
+          ],
+        },
+        {
+          test: /\.css$/,
+          exclude: /\.module\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(eot|otf|woff|woff2|ttf)?$/,
+          use: ['file-loader'],
+        },
+        {
+          test: /\.(bmp|gif|jpe?g|png)?$/,
+          use: ['file-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new ErrorOverlayPlugin(),
+      new HtmlWebPackPlugin({
+        template: paths.html,
+        filename: 'iframe.html',
+      }),
+      new webpack.DefinePlugin({
+        'process.env.templatesDirectory': JSON.stringify(templatesDirectory),
+      }),
     ],
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new ErrorOverlayPlugin(),
-    new HtmlWebPackPlugin({
-      template: paths.html,
-      filename: 'iframe.html',
-    }),
-    new webpack.DefinePlugin({
-      'process.env.templatesDirectory': JSON.stringify(templatesDirectory),
-    }),
-  ],
-});
+  };
+};
