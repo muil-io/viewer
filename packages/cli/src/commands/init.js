0155b9968bb68c56ea02eb6152e9dfb6baf06fe3
Shahaf Shaked <shahaf@live.com>
2020-12-02 17:39:54 +0200

    Update init.js

--- packages/cli/src/commands/init.js
+++ packages/cli/src/commands/init.js
@@ -28,7 +28,7 @@ export default async ({ useNpm, templatesDirectory }) => {
   writePackageJson(packageJson);
   logger.infoSuccess();
 
-  fs.appendFileSync('.gitignore', '\n# muil build folder\n.muil/build\n');
+  fs.appendFileSync('.gitignore', '\n# muil build folder\n.muil/build\n.muil/temp\n');
 
   if (
     !fs.existsSync(templatesDirectory || './templates') &&
