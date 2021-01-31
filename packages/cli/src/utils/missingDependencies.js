50d6e75c501886d61a3f73dc3b4804b38c2cf0e1
Shahaf Shaked <shahaf@live.com>
2021-01-31 19:06:21 +0200

    Update missingDependencies.js

--- packages/cli/src/utils/missingDependencies.js
+++ packages/cli/src/utils/missingDependencies.js
@@ -1,16 +1,31 @@
 const MUST_INCLUDE_DEPENDENCIES = ['react', 'react-dom'];
 
 const missingDependencies = (packageJson) => {
-  const missing = MUST_INCLUDE_DEPENDENCIES.filter(
-    (dependency) =>
-      (!packageJson.dependencies && !packageJson.devDependencies) ||
-      !packageJson.dependencies[dependency] ||
-      !packageJson.devDependencies[dependency],
-  );
+  const missing = MUST_INCLUDE_DEPENDENCIES.filter((dependency) => {
+    let found = false;
+    if (packageJson.dependencies) {
+      found = packageJson.dependencies[dependency];
+    }
+    if (packageJson.devDependencies) {
+      found = found || packageJson.devDependencies[dependency];
+    }
+
+    return !found;
+  });
 
   const isReactScriptsInstalled = packageJson.dependencies && packageJson.dependencies['react-scripts'];
   if (!isReactScriptsInstalled) {
-    return [...missing, 'babel-loader'];
+    let found = false;
+    if (packageJson.dependencies) {
+      found = packageJson.dependencies['babel-loader'];
+    }
+    if (packageJson.devDependencies) {
+      found = found || packageJson.devDependencies['babel-loader'];
+    }
+
+    if (!found) {
+      missing.push('babel-loader');
+    }
   }
 
   return missing;
