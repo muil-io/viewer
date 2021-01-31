7c52ca9aead86615ec4510be6d09f6b64f8aed86
Shahaf Shaked <shahaf@live.com>
2021-01-31 19:17:38 +0200

    Update missingDependencies.js

--- packages/cli/src/utils/missingDependencies.js
+++ packages/cli/src/utils/missingDependencies.js
@@ -14,20 +14,25 @@ const missingDependencies = (packageJson) => {
   });
 
   const isReactScriptsInstalled = packageJson.dependencies && packageJson.dependencies['react-scripts'];
+  console.log('isReactScriptsInstalled', isReactScriptsInstalled);
   if (!isReactScriptsInstalled) {
     let found = false;
+    console.log('packageJson.dependencies', packageJson.dependencies);
     if (packageJson.dependencies) {
       found = packageJson.dependencies['babel-loader'];
     }
+    console.log('packageJson.devDependencies', packageJson.devDependencies);
     if (packageJson.devDependencies) {
       found = found || packageJson.devDependencies['babel-loader'];
     }
 
+    console.log('found', found);
     if (!found) {
       missing.push('babel-loader');
     }
   }
 
+  console.log('missing', missing);
   return missing;
 };
 
